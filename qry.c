de8369883d15ef60dc851b97e69ddc60c9c7706b
Geroldor <geraldo.antonio@uel.br>
2023-03-25 18:53:52 -0300

    montando qry

--- qry.c
+++ qry.c
@@ -3,9 +3,30 @@
 void readqry(archive q){
     FILE *qry = (FILE *) q;
     char *command = NULL;
-    char *type;
-    int i;
-    float r, p, h;
+    char* type;
     fgets(command, 100, qry);
-    sscanf(command, "%c %d %f %f %f", &type, &i, &r, &p, &h);
+    switch(command[0]){
+        case 'mv':
+            int i;
+            float dx, dy;
+            sscanf(command, "%s %d %f %f", type, &i, &dx, &dy);
+        break;
+
+        case 'g':
+            int i;
+            float grs;
+            sscanf(command, "%s %d %f", type, &i, &grs);
+        break;
+
+        case 'ff':
+            int i;
+            float r, p, h;
+            sscanf(command, "%c %d %f %f %f", &type, &i, &r, &p, &h);
+        break;
+
+        case 'tf':
+            int i, l;
+            sscanf(command, "%c %d %f %f %f", &type, &i, &r, &p, &h);
+        break;
+    }
 }
