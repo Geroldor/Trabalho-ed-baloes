fa068cdf2bd9462c53e253ebaf7da01e90db6c5e
Geroldor <geraldo.antonio@uel.br>
2023-04-08 00:37:12 -0300

    paia

--- fila.h
+++ fila.h
@@ -8,24 +8,24 @@ typedef void* fila;
 fila createFila(int capacidade);
 /* cria uma fila circular estatica com a capacidade informada */
 
-void insertFila(Fila *f, item conteudo);
+void insertFila(fila *f, item conteudo);
 /* insere o conteudo na ultima posição disponivel na fila f */
 
-void removeFila(Fila *f);
+void removeFila(fila *f);
 /* remove o primeiro item da fila f */
 
-item getFirstFila(Fila *f);
+item getFirstFila(fila *f);
 /* retorna o primeiro item da fila f */
 
-item getLastFila(Fila *f);
+item getLastFila(fila *f);
 /* retorna o ultimo item da fila f */
 
-int getSizeFila(Fila *f);
+int getSizeFila(fila *f);
 /* retorna a quantidade de elementos na fila f */
 
-int getCapacityFila(Fila *f);
+int getCapacityFila(fila *f);
 /* retorna a capacidade maxima de elementos que a fila pode armazenar */
 
-void killFila(Fila *f);
+void killFila(fila *f);
 /* apaga a fila e seus elementos e libera seus espaços de memoria */
 #endif
--- geo.c
+++ geo.c
@@ -5,8 +5,10 @@
 #include "circle.h"
 #include "rect.h"
 #include "text.h"
+#include "lista.h"
 
-void readgeo(char* fullpathgeo){
+
+void readgeo(char* fullpathgeo, Lista l){
     FILE *geo = fopen(fullpathgeo, "r");
     char command[100], type;
     char corb[20], corp[20], ancora[20],  conteudo[50], cor[20];
@@ -25,27 +27,24 @@ void readgeo(char* fullpathgeo){
             case 'r':
                 
                 sscanf(command, "%c %d %f %f %f %f %s %s", &type, &id, &x1, &y1, &w, &h, corb, corp);
-                retangulo *rect = createRetangulo(id, x1, y1, w, h, corb, corp);
-                printf("ID RETANGULO: %d \nX: %f \nY: %f \nwidth: %f \nheight: %f \nCorb: %s \nCorP: %s", id, x1, y1, w, h, corb, corp);
+                insertLst(l, createRetangulo(id, x1, y1, w, h, corb, corp));
             break;
 
             case 'c':
                
                 sscanf(command, "%c %d %lf %lf %lf %s %s", &type, &id, &x1, &y1, &r, corb, corp);
-                circulo *c = createCirculo(id, x1, y1, r, corb, corp);
-                printf("ID CIRCULO: %d \nX: %f \nY: %f \nR: %f \nCorb: %s \nCorP: %s", id, x1, y1, r, corb, corp);
+                insertLst(l, createCirculo(id, x1, y1, r, corb, corp));
             break;
 
             case 't': 
                 sscanf(command, "%c %d %f %f %s %s %s", &type, &id, &x1, &y1, corb, corp, conteudo);
-                texto *t = textoCreate(id, x1, y1, conteudo, ancora, corb, corp);
+                insertLst(l, textoCreate(id, x1, y1, conteudo, ancora, corb, corp));
                 printf("ID TEXTO: %d \nX: %f \nY: %f \nConteudo: %s \nCorb: %s \nCorP: %s \nAncora: %s", id, x1, y1, conteudo, corb, corp, ancora);
             break;
 
             case 'l':
-                sscanf(command, "%c %d %lf %lf %lf %lf %s", &type, &id, &x1, &y1, &x2, &y2, cor);
-                linha *l = createLinha(id, x1, y1, x2, y2, cor);
-                printf("ID LINHA: %d \nX1: %f \nY1: %f \nX2: %f \nY2: %f \nCor: %s", id, x1, y1,x2 , y2, r, corb);
+                sscanf(command, "%c %d %f %f %f %f %s", &type, &id, &x1, &y1, &x2, &y2, cor);
+                insertLst(l, createLinha(id, x1, y1, x2, y2, cor));
             break;
         }
     }
@@ -53,6 +52,9 @@ void readgeo(char* fullpathgeo){
 }
 
 int main(){
-    char fullpathgeo[] = "~/Downloads/t1/00-smpls-caca-disp.geo";
-    readgeo(fullpathgeo);
+    Lista l = createLst(-1);
+    char fullpathgeo[100] = "~/Downloads/t1/00-smpls-caca-disp.geo";
+    readgeo(fullpathgeo, l);
+    killLst(l);
+    return 0;
 }
